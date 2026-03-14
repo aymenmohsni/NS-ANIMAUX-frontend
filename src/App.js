c0410d6f11fe1df16a4b0239c5fba6f6e051cb24
Aymen Mohsni <aymenmohsnitn@gmail.com>
2026-03-14 18:12:32 +0100

    some changes

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
-import men_banner from './Components/Assets/banner_mens.png'
-import women_banner from './Components/Assets/banner_women.png'
-import kid_banner from './Components/Assets/banner_kids.png'
 import PlaceOrder from './Pages/PlaceOrder';
 import Profile from './Pages/Profile';
 import Terms from './Pages/Terms';
@@ -22,10 +19,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Accueil/>} />
-          <Route path="/chat" element={<ShopCategory banner={men_banner} category="chat"/>} />
-          <Route path="/chien" element={<ShopCategory banner={women_banner} category="chien"/>} />
-          <Route path="/poissons" element={<ShopCategory banner={kid_banner} category="poissons"/>} />
-          <Route path="/oiseaux" element={<ShopCategory banner={men_banner} category="oiseaux"/>} />
+          <Route path="/chat" element={<ShopCategory category="chat"/>} />
+          <Route path="/chien" element={<ShopCategory category="chien"/>} />
+          <Route path="/poissons" element={<ShopCategory category="poissons"/>} />
+          <Route path="/oiseaux" element={<ShopCategory category="oiseaux"/>} />
 
           <Route path="/product/:productId" element={<Product/>} />
           <Route path="/cart" element={<Cart/>} />
